dd7aa6a09922e05cf7f526a756ded086105e9a5c
pebrianz <pebrianz117@gmail.com>
2023-01-05 15:48:59 +0700

    icon pwa

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,12 @@ export default defineConfig({
         scope: "./",
         icons: [
           {
-            src: "android-chrome-192x192.png",
+            src: "bird-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "android-chrome-512x512.png",
+            src: "bird-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
