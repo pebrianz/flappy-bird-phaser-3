b531a1f0191d9a33211436b405db0ea7c1222659
pebrianz <pebrianz117@gmail.com>
2023-01-05 14:12:03 +0700

    progresive web app

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: './', // work
-})
+  base: "./", // work
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "flappy-bird",
+        short_name: "flappy-bird",
+        start_url: "./",
+        display: "standalone",
+        background_color: "#ffffff",
+        lang: "en",
+        scope: "./",
+      },
+    }),
+  ],
+});
