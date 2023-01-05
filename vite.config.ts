988ed96959c2e25d4f4c1092c275215ac764764f
pebrianz <pebrianz117@gmail.com>
2023-01-05 15:36:37 +0700

    icon pwa

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,28 @@ export default defineConfig({
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
       manifest: {
-        name: "flappy-bird",
-        short_name: "flappy-bird",
+        name: "Flappy Bird",
+        short_name: "Flappy Bird",
         start_url: "./",
         display: "standalone",
+        theme_color: "#ffffff",
         background_color: "#ffffff",
         lang: "en",
         scope: "./",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
